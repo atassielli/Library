aea914c8864ba6028cba3e8fe86d9835303f5d48
atassielli <anthonytassielli@gmail.com>
2022-08-03 20:58:35 -0700

    Add button for adding a book to your library and defer script
    defer tag on html file for running script after html and adding a new book button to the DOM

--- constructor.js
+++ constructor.js
@@ -1,3 +1,4 @@
+let myLibrary = [];
 
 function Book (title, author, pages, read) {
     this.title = title;
@@ -8,7 +9,18 @@ function Book (title, author, pages, read) {
         return `${title} by ${author}, ${pages} pages, ${read}`
     }
 }
-
 const book1 = new Book('Star Wars', 'George Lucas', 457, 'Have read')
 
 console.log(book1.info())
+
+//Create button to bring up a form that asks the user for the details of a new book //
+
+const body = document.querySelector('.container');
+
+console.log(body)
+
+const newBookButton = document.createElement('button');
+newBookButton.classList.add('button');
+newBookButton.textContent = 'New Book';
+body.appendChild(newBookButton);
+
