d0a40706149cebb47d54c971c2f449453b68dabb
atassielli <anthonytassielli@gmail.com>
2022-08-07 17:24:05 -0700

    Add event listener to new book button that gets imput from user and adds book to library array
    prompts collect the users input to add a new book to the library array

--- constructor.js
+++ constructor.js
@@ -9,18 +9,34 @@ function Book (title, author, pages, read) {
         return `${title} by ${author}, ${pages} pages, ${read}`
     }
 }
-const book1 = new Book('Star Wars', 'George Lucas', 457, 'Have read')
 
-console.log(book1.info())
+let newTitle;
+let newAuthor;
+let newPages;
+let newread;
+
+function addBook () {
+    let newBook = new Book (newTitle, newAuthor, newPages, newread);
+    myLibrary.push(newBook);
+}
 
 //Create button to bring up a form that asks the user for the details of a new book //
 
-const body = document.querySelector('.container');
-
-console.log(body)
+const addBooks = document.querySelector('.addBook');
 
 const newBookButton = document.createElement('button');
 newBookButton.classList.add('button');
 newBookButton.textContent = 'New Book';
-body.appendChild(newBookButton);
+addBooks.appendChild(newBookButton);
+
+newBookButton.addEventListener('click', function () {
+    newTitle = window.prompt('Title:');
+    newAuthor = window.prompt('Author:');
+    newPages = window.prompt('Number of Pages:');
+    newread = window.prompt('Have you finished this book?')
+    addBook();
+    console.log(myLibrary)
+})
+
+
 
