c8301be47b30a408a6b511db0bcd4aef5239e294
atassielli <anthonytassielli@gmail.com>
2022-08-09 20:50:38 -0700

    Create DOM elements to track user book entries and place them into the library card display
    each object element links to a DOM element that gets placed into the display cards to show the books in the users library

--- constructor.js
+++ constructor.js
@@ -17,20 +17,37 @@ let newread;
 
 function addBook () {
     let newBook = new Book (newTitle, newAuthor, newPages, newread);
-    myLibrary.push(newBook.info());
-    console.log(myLibrary)
+    myLibrary.push(newBook);
     storeBook();
 }
 
 function storeBook () {
     let newCard = document.createElement('div');
-    newCard.classList.add('.book');
+    newCard.classList.add('book');
     library.appendChild(newCard);
-    let bookInfo = document.createElement('div');
-    bookInfo.classList.add('.bookInfo');
-    bookInfo.textContent = `${myLibrary[myLibrary.length-1]}`;
-    newCard.appendChild(bookInfo);
-
+    newCard.classList.add(`${myLibrary[myLibrary.length-1].pages}`);
+    let bookTitle = document.createElement('div');
+    bookTitle.classList.add('bookTitle')
+    bookTitle.textContent = `Title: ${myLibrary[myLibrary.length-1].title}`;
+    newCard.appendChild(bookTitle);
+    let bookAuthor = document.createElement('div');
+    bookAuthor.classList.add('bookAuthor')
+    bookAuthor.textContent = `Author: ${myLibrary[myLibrary.length-1].author}`;
+    newCard.appendChild(bookAuthor);
+    let bookPages = document.createElement('div');
+    bookPages.classList.add('bookPages')
+    bookPages.textContent = `Pages: ${myLibrary[myLibrary.length-1].pages}`;
+    newCard.appendChild(bookPages);
+    let bookread = document.createElement('button');
+    bookread.classList.add('bookread')
+    bookread.classList.add('cardButton')
+    bookread.textContent = `Completed: ${myLibrary[myLibrary.length-1].read}`;
+    newCard.appendChild(bookread);
+    let bookDelete = document.createElement('button');
+    bookDelete.classList.add('bookDelete')
+    bookDelete.classList.add('cardButton')
+    bookDelete.textContent = 'Delete';
+    newCard.appendChild(bookDelete);
 }
 
 //Create button to bring up a form that asks the user for the details of a new book //
@@ -39,7 +56,7 @@ const addBooks = document.querySelector('.addBook');
 
 const newBookButton = document.createElement('button');
 newBookButton.classList.add('button');
-newBookButton.textContent = 'New Book';
+newBookButton.textContent = 'Add Book';
 addBooks.appendChild(newBookButton);
 
 newBookButton.addEventListener('click', function () {
@@ -52,11 +69,16 @@ newBookButton.addEventListener('click', function () {
 })
 
 function validateStatus() {
-    while (newread.toLowerCase() != 'yes' && newread.toLowerCase != 'no') {
+    while (newread.toLowerCase() != 'yes' && newread.toLowerCase() != 'no') {
         newread = window.prompt('You must answer Yes or No. Have you read this book?')
     }
 }
 
-const library = document.querySelector('.yourBooks');
+const library = document.querySelector('.cardContainer');
 
 
+const empty = document.querySelector('.empty');
+
+empty.addEventListener('click', function () {
+    location.reload();
+})
