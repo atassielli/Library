915fa8ae7e027869ebe2aa9cc163c16e9aa99db9
atassielli <anthonytassielli@gmail.com>
2022-09-08 22:09:10 -0700

    Refactor object constructor for books into a class
    book class built to replace object constructor without changing user functionality

--- constructor.js
+++ constructor.js
@@ -1,11 +1,15 @@
 let myLibrary = [];
 
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
+class Book {
+    
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
         return `${title} by ${author}, ${pages} pages, ${read}`
     }
 }
